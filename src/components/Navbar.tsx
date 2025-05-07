87f508d153d89708adf1905af633e8441d8d9a2b
Thyme2be <trinchuaynugoon@gmail.com>
2025-05-07 17:46:33 +0700

    create navbar drawer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import dynamic from "next/dynamic";
 import type { LottieRefCurrentProps } from "lottie-react";
 import Image from "next/image";
 import MenuLottie from "../../public/menu-lottie.json";
+import Link from "next/link";
 
 const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
@@ -29,10 +30,17 @@ export default function Navbar() {
     setIsOpen(!isOpen);
   };
 
+  const handleLinkClick = () => {
+    if (!lottieRef.current) return;
+    lottieRef.current.setSpeed(4.5);
+    lottieRef.current.playSegments([45, 0], true);
+    setIsOpen(false);
+  };
+
   return (
     <header className="flex justify-center max-sm:justify-end">
       {/* Mobile nav */}
-      <nav className="sm:hidden h-10 w-10">
+      <nav className="sm:hidden h-10 w-10 z-10">
         <div onClick={handleMenuClick}>
           <Lottie
             lottieRef={lottieRef}
@@ -43,6 +51,20 @@ export default function Navbar() {
           />
         </div>
       </nav>
+
+      {/* Mobile drawer */}
+      <div
+        className={`sm:hidden absolute left-0 top-0 w-full bg-primary-gradient transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-y-0 shadow-secondary shadow-lg/40 " : "-translate-y-full"
+        }`}
+      >
+        <ul className="flex flex-col items-center gap-4 py-6 text-lg font-semibold">
+          <Link href={"/"}><li className={linkClassName} onClick={handleLinkClick}>Home</li></Link>
+          <li className={linkClassName}>Major</li>
+          <li className={linkClassName}>Sponsors</li>
+          <li className={linkClassName}>FAQ</li>
+        </ul>
+      </div>
 
       {/* Desktop nav */}
       <nav className="max-sm:hidden flex rounded-full items-center bg-primary-gradient shadow-primary-gradient py-3 px-8 gap-5">
